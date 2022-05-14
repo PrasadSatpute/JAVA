031fb763f76f65cdc6dc0df933da6f9fe95841f3
PrasadSatpute <prasadsatpute000@gmail.com>
2022-05-14 20:56:39 +0530

    Without Collection Array Index

--- Core Java/Multithreading/Interview/src/interview/ArrayOccurenceIndex11.java
+++ Core Java/Multithreading/Interview/src/interview/ArrayOccurenceIndex11.java
@@ -1,49 +1,31 @@
 
 package interview;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-
 public class ArrayOccurenceIndex11 {
     public static void main(String[] args) {
         int arr [] = {1,2,6,7,3,4,3,5,5,8,4};
         
+        int minElement = 0;
+        int index = 0;
         
-        
-        HashSet<Integer> map = new HashSet<>();
-        
-        for (int i : arr) {
-            map.add(i);
-        }
-        int minElement = Collections.min(map);
-        System.out.println(map.toString());
-        
-        for (Integer integer : map) {
-            if(minElement == integer){
-                
+        for(int i = 0; i < arr.length; i++){
+            for(int j = i+1; j < arr.length; j++){
+                if(arr[i] == arr[j]){
+                    if(minElement != 0){
+                        if(minElement > arr[i]){
+                            minElement = arr[i];
+                            index = i;
+                        }
+                    }
+                    else
+                    {
+                        minElement = arr[i];
+                        index = i;
+                    }
+                }  
             }
         }
-        
-//        for(int i = 0; i < arr.length; i++){
-//            for(int j = i+1; j < arr.length; j++){
-//                if(arr[i] == arr[j]){
-//                    map.add(arr[i]);
-//                }  
-//            }
-//        }
-//        
-//        int minElement = Collections.min(map);
-//        int index = 0;
-//        
-//        for (int i = 0; i < arr.length; i++) {
-//            if(minElement == arr[i]){
-//                index=i;
-//                break;
-//            }
-//        }
-//        
-//        System.out.println("Min Element = "+minElement);
-//        System.out.println("Index = "+index);
+        System.out.println("Min Element = "+minElement);
+        System.out.println("Index = "+index);
     }
 }
