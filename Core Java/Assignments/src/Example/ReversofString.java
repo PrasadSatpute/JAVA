2fe73be1d3c215c246ba835f15e6318b992c4eee
PrasadSatpute <prasadsatpute000@gmail.com>
2022-05-15 17:47:55 +0530

    Revers of String, Input as sum of two Prime Number

---
+++ Core Java/Assignments/src/Example/ReversofString.java
@@ -0,0 +1,20 @@
+
+package Example;
+
+public class ReversofString {
+    public static void main(String[] args) {
+        String str = "1 2 3 4 5 6 7 8 9 10";
+        
+        char[] revString = new char[str.length()];
+        
+        System.out.println("Original String = "+str.toString());
+        
+        int j = 0;
+        for (int i = str.length()-1; i >= 0; i--) {
+            revString[j] = str.charAt(i);
+            j++;
+        }
+        
+        System.out.println("Revers String = "+String.valueOf(revString));
+    }
+}
