67bdb315fc04f389c7dd1b5a374307ae7d951af3
PrasadSatpute <prasadsatpute000@gmail.com>
2022-05-17 14:45:34 +0530

    update ArrayOccurenceIndex111

--- Core Java/Multithreading/Interview/src/interview/ArrayOccurenceIndex111.java
+++ Core Java/Multithreading/Interview/src/interview/ArrayOccurenceIndex111.java
@@ -2,25 +2,53 @@
 package interview;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import jdk.nashorn.internal.ir.ContinueNode;
 
 public class ArrayOccurenceIndex111 {
     public static void main(String[] args) {
         
-        int arr [] = {1,2,6,7,3,4,3,5,5,8,4};
+//        Stream<Integer> number = Stream.of(1, 2, 3, 4, 5);
+//
+//        List<Integer> result2 = number.filter(x -> x != 3).collect(Collectors.toList());
+//
+//        result2.forEach(x -> System.out.println(x));
         
-        List<Integer> list = new ArrayList<>();
-        for (int i : arr) {
-            list.add(i);
+        int arr [] = {1,2,6,7,3,4,3,5,5,8,4};
+        List<Integer> list = Arrays.stream(arr).boxed().collect(Collectors.toList());
+        
+        
+        System.out.println(list);
+        
+        Stream<Integer> list1 = list.stream().filter(i -> Collections.frequency(list, i) > 1);
+        list1.forEach(System.out::print);
+        
+        //Convert a Stream to List
+        List<Integer> list2 = list1.collect(Collectors.toList());
+        
+        int min = 0;
+        try {
+            min = Collections.min(list2);
+            
+        } catch (Exception e) {
+            System.out.println("Min Element = "+min);
+            for (int i = 0; i < arr.length; i++) {
+                if (min == arr[i]) {
+                    System.out.println("Min Element Index = "+i);
+                    break;
+                }
+            }
         }
         
         list.stream()
-        .filter(i -> Collections.frequency(list, i) > 1 ) 
+        .filter(i -> Collections.frequency(list, i) > 1 && Collections.min(list) < i) 
         .collect(Collectors.toSet())
         .forEach(System.out::print);
-        
-        
     }
 }
