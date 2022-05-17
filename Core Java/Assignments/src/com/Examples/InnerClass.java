a9be4b9e78fafa8357c622e38d03efce7e8d8d2e
PrasadSatpute <prasadsatpute000@gmail.com>
2022-05-17 19:04:09 +0530

    Static Method

--- Core Java/Assignments/src/com/Examples/InnerClass.java
+++ Core Java/Assignments/src/com/Examples/InnerClass.java
@@ -4,22 +4,37 @@ package com.Examples;
 class Outer{
     
     void callOuter(){
-        System.out.println("Call Outer Class");
+        System.out.println("Call Outer Class Method Using Object");
+    }
+    
+    static void callOuter1(){
+        System.out.println("Call Outer Static Class Method Using ClassName");
     }
     
     class Inner{
         void callInner(){
-            System.out.println("Call Inner Class");
+            System.out.println("Call Inner Class Method");
+        }
+    }
+    
+    static class Inner1{
+        void callInner(){
+            System.out.println("Call Static Inner Class Method");
         }
     }
 }
 
 
 public class InnerClass {
+    
+    
     public static void main(String[] args) {
         Outer out = new Outer();
         out.callOuter();
+        Outer.callOuter1();
         Outer.Inner in = out.new Inner();
         in.callInner();
+        Outer.Inner1 in1 = new Outer.Inner1();
+        in1.callInner();
     }
 }
