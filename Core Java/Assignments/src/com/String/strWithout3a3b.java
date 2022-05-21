5e48f734cb8eb1fa3a54f3867684ed29cd5d418e
PrasadSatpute <prasadsatpute000@gmail.com>
2022-05-21 21:42:56 +0530

    String Without 3a 3b

---
+++ Core Java/Assignments/src/com/String/strWithout3a3b.java
@@ -0,0 +1,35 @@
+
+package com.String;
+class Solution {
+    public String strWithout3a3b(int A, int B) {
+        StringBuilder ans = new StringBuilder();
+
+        while (A > 0 || B > 0) {
+            boolean writeA = false;
+            int L = ans.length();
+            if (L >= 2 && ans.charAt(L-1) == ans.charAt(L-2)) {
+                if (ans.charAt(L-1) == 'b')
+                    writeA = true;
+            } else {
+                if (A >= B)
+                    writeA = true;
+            }
+
+            if (writeA) {
+                A--;
+                ans.append('a');
+            } else {
+                B--;
+                ans.append('b');
+            }
+        }
+
+        return ans.toString();
+    }
+}
+public class strWithout3a3b {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.strWithout3a3b(5, 5));
+    }
+}
--- Core Java/Assignments/src/com/multithread/Synchronization/SequenceNumber.java
+++ Core Java/Assignments/src/com/multithread/Synchronization/SequenceNumber.java
@@ -1,9 +1,71 @@
-
 package com.multithread.Synchronization;
-class PrintSequnceNumber
-{
-    while
+
+class PrintSequnceNumber {
+
+    private int start = 1;
+    private int totalNumber;
+
+    public PrintSequnceNumber(int totalNumber) {
+        this.totalNumber = totalNumber;
+    }
+
+    public synchronized void printNumber(int totalThread, int threadNumber) {
+        while (start <= totalNumber) {
+            if (start != threadNumber && start <= totalThread) {
+                try {
+                    wait();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else {
+                System.out.println(Thread.currentThread().getName() + " " + start++);
+            }
+            notifyAll();
+        }
+    }
 }
+
+class CountrThread implements Runnable {
+
+    PrintSequnceNumber printSeqNum;
+    private int totalThread;
+    private int threadNumber;
+
+    public CountrThread(PrintSequnceNumber printSeqNum, int totalThread, int threadNumber) {
+        this.printSeqNum = printSeqNum;
+        this.totalThread = totalThread;
+        this.threadNumber = threadNumber;
+    }
+
+    @Override
+    public void run() {
+        printSeqNum.printNumber(totalThread, threadNumber);
+    }
+}
+
 public class SequenceNumber {
-    
+
+    public static void main(String[] args) {
+        PrintSequnceNumber psn = new PrintSequnceNumber(5);
+
+        CountrThread one = new CountrThread(psn, 5, 1);
+        CountrThread two = new CountrThread(psn, 5, 2);
+        CountrThread three = new CountrThread(psn, 5, 3);
+        CountrThread four = new CountrThread(psn, 5, 4);
+        CountrThread five = new CountrThread(psn, 5, 5);
+
+        Thread t1 = new Thread(one, "One");
+        Thread t2 = new Thread(two, "two");
+        Thread t3 = new Thread(three, "three");
+        Thread t4 = new Thread(four, "four");
+        Thread t5 = new Thread(five, "five");
+        
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+        t5.start();
+        
+    }
+
 }
