1d818be1a25ffac7092d592d4745e3d6cfc476a6
PrasadSatpute <prasadsatpute000@gmail.com>
2022-05-24 08:18:30 +0530

    Anonymous Inner Class

--- Core Java/Assignments/src/com/Examples/AnonymousInnerClass.java
+++ Core Java/Assignments/src/com/Examples/AnonymousInnerClass.java
@@ -1,8 +1,29 @@
 
 package com.Examples;
 
+//class Other{
+//    void call(){
+//        System.out.println("Other class Call");
+//    }
+//    void call1(){
+//        System.out.println("Other class Call");
+//    }
+//}
+//
+//public class AnonymousInnerClass {
+//    public static void main(String[] args) {
+//        Other o = new Other();
+//        o.call();
+//    }
+//}
+
+//-----------------------------------------------------------------------
+
 class Other{
     void call(){
+        System.out.println("Other class Call");
+    }
+    void call1(){
         System.out.println("Other class Call");
     }
 }
@@ -10,11 +31,56 @@ class Other{
 public class AnonymousInnerClass {
     public static void main(String[] args) {
         Other o = new Other(){
-            void call(){
-                System.out.println("Anonymous Call");
-            }
+            
         };
-        
         o.call();
+        o.call1();
     }
 }
+
+//-----------------------------------------------------------------------
+
+//interface Other{
+//    void call();
+//}
+
+//public class AnonymousInnerClass {
+//    public static void main(String[] args) {
+//        Other o = new Other(){
+//            public void call(){
+//                System.out.println("Anonymous Call");
+//            }
+//        };
+//        o.call();
+//    }
+//}
+
+//-----------------------------------------------------------------------
+
+//interface Other{
+//    void call();
+//    void add(int a ,int b);
+//    int sub(int x, int y);
+//}
+//
+//public class AnonymousInnerClass {
+//    public static void main(String[] args) {
+//        Other o = new Other(){
+//            @Override
+//            public void call() {
+//                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//            }
+//
+//            @Override
+//            public void add(int a, int b) {
+//                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//            }
+//
+//            @Override
+//            public int sub(int x, int y) {
+//                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//            }
+//            
+//        };
+//    }
+//}
