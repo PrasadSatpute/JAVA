6f4892b315e62632b845f849f476f8095ad33e59
PRASAD PURUSHOTTAM SATPUTE <32232749+PrasadSatpute@users.noreply.github.com>
2023-04-17 12:14:30 +0530

    leading and trailing

--- Core Java/Multithreading/Interview/src/interview/FindNumberStartingWith1.java
+++ Core Java/Multithreading/Interview/src/interview/FindNumberStartingWith1.java
@@ -8,6 +8,15 @@ public class FindNumberStartingWith1 {
 	public static void main(String[] args) {
 		List<Integer> arrayList = Arrays.asList(10,13,8,49,17,96,31);
 		arrayList.stream().map(s -> s + "").filter(s -> s.startsWith("1")).forEach(System.out::println);;
+		
+		
+//		How do you remove leading and trailing spaces from a string in Java?
+		
+		String s = "  abc  def\t";
+		
+		s = s.strip();
+				
+		System.out.println(s);
 	}
 
 }
