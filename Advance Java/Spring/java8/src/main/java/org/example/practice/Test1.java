af23234d6b5df89e0ca8cd8c343622c4da622bcc
PRASAD PURUSHOTTAM SATPUTE <32232749+PrasadSatpute@users.noreply.github.com>
2024-03-21 22:19:14 +0530

    Lambda

---
+++ Advance Java/Spring/java8/src/main/java/org/example/practice/Test1.java
@@ -0,0 +1,8 @@
+package org.example.practice;
+import java.util.List;
+//Use the Stream API to filter a list of strings starting with the letter "A".
+public class Test1 {
+    public static void main(String[] args) {
+
+    }
+}
