75dab9083e3410b360a06c3730971d24227e0b72
PRASAD PURUSHOTTAM SATPUTE <32232749+PrasadSatpute@users.noreply.github.com>
2024-03-29 11:57:04 +0530

    Update

--- Advance Java/Spring/java8/src/main/java/org/example/interview2/P0_String_Integer.java
+++ Advance Java/Spring/java8/src/main/java/org/example/interview2/P0_String_Integer.java
@@ -1,9 +1,9 @@
 package org.example.interview2;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 public class P0_String_Integer {
     public static void main(String[] args) {
@@ -19,6 +19,25 @@ public class P0_String_Integer {
                 .mapToObj(ch -> (char) ch)
                 .collect(Collectors.toList())
                 .forEach(System.out::println);
+
+        System.out.println("Print each char in UpperCase");
+        name.toUpperCase()
+                .chars()
+                .mapToObj(ch -> (char)ch);
+
+        System.out.println("Print each char in Revers Order");
+        name
+                .chars()
+                .mapToObj(ch -> (char)ch)
+                .sorted(Comparator.reverseOrder())
+                .forEach(System.out::print);
+
+        System.out.println("Print Frequency of Each Char in String");
+        Map<Character, Long> collect = name
+                .chars()
+                .mapToObj(ch -> (char) ch)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        System.out.println(collect);
 
     }
 }
--- Advance Java/Spring/java8/src/main/java/org/example/interview2/P6_Join_Prefix_Suffix.java
+++ Advance Java/Spring/java8/src/main/java/org/example/interview2/P6_Join_Prefix_Suffix.java
@@ -6,6 +6,7 @@ public class P6_Join_Prefix_Suffix {
     public static void main(String[] args) {
         System.out.println("Given a list of strings, join the strings with ‘[‘ as prefix, ‘]’ as suffix and ‘,’ as delimiter?");
         List<String> listOfStrings = Arrays.asList("Facebook", "Twitter", "YouTube", "WhatsApp", "LinkedIn");
+        System.out.println(listOfStrings);
         String joinedString = listOfStrings.stream().collect(Collectors.joining(", ", "[", "]"));
         System.out.println(joinedString);
     }
